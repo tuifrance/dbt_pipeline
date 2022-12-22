886c4fc52899a0727b671cfc3249c1a8e585ee64
tui@data-hanalytics.io <tui@data-hanalytics.io>
2022-12-22 15:32:06 +0000

    update data pipeline with assisted conversion in incremental

--- models/google_analytics/traffic/stg_mcf_assisted_conversions.sql
+++ models/google_analytics/traffic/stg_mcf_assisted_conversions.sql
@@ -1,7 +1,7 @@
 
 {{
     config(
-        materialized='table',
+        materialized='incremental',
         labels={
             'type': 'google_analytics',
             'contains_pie': 'no',
@@ -9,7 +9,7 @@
         },
     )
 }}
-
+with consolidation as (
 select
     conversion_date as date,
     source,
@@ -20,12 +20,9 @@ select
         then 'SEA Brand & Hotel'
         when campaign_name not like '%BRAND%' and medium = 'cpc'
         then 'SEA Generic'
-        when
-            medium = 'organic'
-            or medium = 'qwant.com'
-            or medium like '%yahoo.com%'
+        when medium = 'organic' or medium = 'qwant.com' or medium like '%yahoo.com%'
         then 'SEO'
-        when channel_grouping = 'E-CRM' or source ='ecrm'
+        when channel_grouping = 'E-CRM' or source = 'ecrm'
         then 'ECRM'
         when channel_grouping = 'Comparateur'
         then 'Comparateur'
@@ -35,37 +32,32 @@ select
             or medium = 'affiliation'
             and source != 'EPERFLEX'
         then 'Affiliation'
-        when
-            medium = 'retargeting'
-            and source = 'CRITEO'
-            or source = 'EPERFLEX'
+        when medium = 'retargeting' and source = 'CRITEO' or source = 'EPERFLEX'
         then 'Retargeting Display'
-        when
-            source = 'Facebookads'
-            and medium in ('retargeting', 'Retargeting')
+        when source = 'Facebookads' and medium in ('retargeting', 'Retargeting')
         then 'Retargeting Social'
         when
             medium = 'cpm'
             or campaign_name like '%branding%'
             or medium like '%branding%'
         then 'Display Branding'
-        when
-            source = 'Facebookads'
-            and medium not in ('retargeting', 'Retargeting')
+        when source = 'Facebookads' and medium not in ('retargeting', 'Retargeting')
         then 'Paid Social'
         when
-            channel_grouping = 'Social'
-            and medium not in ('retargeting', 'Retargeting')
+            channel_grouping = 'Social' and medium not in ('retargeting', 'Retargeting')
         then 'Social'
-        when
-            channel_grouping in ('Accès Direct', 'Référents')
-            and source != 'qwant.com'
+        when channel_grouping in ('Accès Direct', 'Référents') and source != 'qwant.com'
         then 'Direct'
-        else
-            'Autre'
-        end as customchannel_grouping,    
-            channel_grouping,
-            cast(total_conversions as float64) as conversions,
-            cast(total_conversions_value as float64) as total_conversions_value,
-        from {{ source('assist_conv', 'Assisted_Conversions') }}
-     order by conversion_date desc 
+        else 'Autre'
+    end as customchannel_grouping,
+    channel_grouping,
+    cast(total_conversions as float64) as conversions,
+    cast(total_conversions_value as float64) as total_conversions_value,
+from {{ source('assist_conv', 'Assisted_Conversions') }}
+order by conversion_date desc
+)
+
+select *
+from consolidation
+{% if is_incremental() %} where date > (select max(date) from {{ this }}) {% endif %}
+order by date desc
