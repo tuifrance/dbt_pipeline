44d24b0132787b6ece131fb7a8a6cd9674131a3f
tui@data-hanalytics.io <tui@data-hanalytics.io>
2023-04-21 11:02:20 +0000

    update data

--- models/consolidation/stg_crm_ga_channel_consolidation.sql
+++ models/consolidation/stg_crm_ga_channel_consolidation.sql
@@ -1,10 +1,10 @@
 {{
     config(
-        materialized='table',
+        materialized="table",
         labels={
-            'type': 'cdg_google_analytics',
-            'contains_pie': 'no',
-            'category': 'production',
+            "type": "cdg_google_analytics",
+            "contains_pie": "no",
+            "category": "production",
         },
     )
 }}
@@ -23,7 +23,7 @@ with
             count(
                 distinct case when customer_type = 'New Customer' then id_email_md5 end
             ) as new_customers
-        from {{ ref('stg_crm_ga_consolidation') }}
+        from {{ ref("stg_crm_ga_consolidation") }}
         group by 1, 2, 3
     ),
 
@@ -38,11 +38,11 @@ with
             ga_revenue,
             ga_users,
             ga_new_users,
-            ga_bounces, 
+            ga_bounces,
             final_revenue as revenue_cdg,
             final_ventes as transactions_cdg,
             pax,
-        from {{ ref('stg_ga_cdg_consolidation') }}
+        from {{ ref("stg_ga_cdg_consolidation") }}
 
     ),
 
@@ -55,28 +55,43 @@ with
             impressions,
             clicks,
             cost
-        from {{ ref('stg_media_data_consolidation') }}
-    ), 
-    assisted_conversion as ( 
-      
-        select 
-           date , 
-           channel_grouping, 
-           concat(date, '_', channel_grouping) as unique_id,
-           conversions, 
-           conversions_value
-        from {{ ref('stg_mcf_assisted_conversions_vf') }}
+        from {{ ref("stg_media_data_consolidation") }}
+    ),
+    assisted_conversion as (
+
+        select
+            date,
+            channel_grouping,
+            concat(date, '_', channel_grouping) as unique_id,
+            conversions,
+            conversions_value
+        from {{ ref("stg_mcf_assisted_conversions_vf") }}
+    ),
+
+    gsheet_cost as (
+        select date, 
+                channel_grouping,
+                concat(date, '_', channel_grouping) as unique_id,
+                cost from {{ ref("stg_gsheet_cost_data") }}
     )
 
 select
     data_ga.date,
     data_ga.channelgrouping,
-    case 
-        when data_ga.channelgrouping in ('SEA Generic', 'SEA Brand & Hotel', 'Paid Social' , 'Affiliation', 'Comparateur') then 'PAID MEDIA'
+    case
+        when
+            data_ga.channelgrouping in (
+                'SEA Generic',
+                'SEA Brand & Hotel',
+                'Paid Social',
+                'Affiliation',
+                'Comparateur'
+            )
+        then 'PAID MEDIA'
         else 'EARN'
-        -- when data_ga.channelgrouping in('SEO') then 'SEO'
-        -- when data_ga.channelgrouping in ('ECRM') then 'CRM'
-        -- else 'AUTRES'
+    -- when data_ga.channelgrouping in('SEO') then 'SEO'
+    -- when data_ga.channelgrouping in ('ECRM') then 'CRM'
+    -- else 'AUTRES'
     end as channel_grouping_grouped,
     data_ga.sessions,
     data_ga.transactions,
@@ -85,18 +100,20 @@ select
     data_ga.ga_bounces,
     data_ga.ga_users,
     data_ga.ga_new_users,
-    round(data_ga.revenue_cdg,2) as revenue_cdg ,
-    round(data_ga.transactions_cdg,2) as transactions_cdg ,
+    round(data_ga.revenue_cdg, 2) as revenue_cdg,
+    round(data_ga.transactions_cdg, 2) as transactions_cdg,
     media_data.impressions,
     media_data.clicks,
     media_data.cost,
     data_crm.total_customers,
     data_crm.old_customers,
-    data_crm.new_customers, 
+    data_crm.new_customers,
     assisted_conversion.conversions as assisted_conversions,
     assisted_conversion.conversions_value as assisted_conversions_value,
+    gsheet_cost.cost as gsheet_cost, 
 from data_ga
 left join data_crm on data_ga.unique_id = data_crm.unique_id
 left join media_data on data_ga.unique_id = media_data.unique_id
 left join assisted_conversion on data_ga.unique_id = assisted_conversion.unique_id
+left join gsheet_cost on data_ga.unique_id = gsheet_cost.unique_id
 order by data_ga.date desc
